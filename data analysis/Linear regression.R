b99ae34b21719fa041da873a57eb93deb222a073
Siyuan Shang <cjbbt12@gmail.com>
2021-11-24 21:43:04 -0500

    extract possible atrributes, log transformation of price, R^2=0.87 but unsolved model assumption problem

--- data analysis/Linear regression.R
+++ data analysis/Linear regression.R
@@ -43,7 +43,9 @@ raw_data <- raw_data %>%
                 total_parking = Total.Parking.Spaces,
                 annual_property_tax = Annual.Property.Taxes,
                 features = Features,
-                cooling = Cooling
+                cooling = Cooling,
+                address2 = Address2,
+                amenities_nearby = Amenities.Nearby
                 )
 
 #discard attributes that missing too much value and not useful in linear regression
@@ -52,7 +54,7 @@ realtor_data <- raw_data %>%
                 bathrooms, bedrooms, building_amenities, has_photo, photos, 
                 has_CMHCFTHBBanner, features, cooling, heating_type, community_name, 
                 time_length, above_grade, maintenance_fees, annual_property_tax,
-                exterior_finish
+                exterior_finish, amenities_nearby
                 )
 
 #correct building type
@@ -62,13 +64,17 @@ realtor_data <- realtor_data %>%
     TRUE ~ building_type
   ))
 
+realtor_data <- realtor_data %>%
+  filter(price<3000000 && price>100000)  %>%
+  filter(building_type == 'Apartment')
+
 #group heating type
 realtor_data <- realtor_data %>%
   mutate(heating_type = case_when(
     str_detect(heating_type, 'Baseboard')~ "Baseboard heaters",
     str_detect(heating_type, 'Forced air')~ "Forced air",
     str_detect(heating_type, 'Heat Pump')~ "Heat Pump",
-    str_detect(heating_type, 'radia')~ "Radiant heat",
+    str_detect(heating_type, 'Radia|radia')~ "Radiant heat",
     TRUE ~ heating_type
   ))
 
@@ -88,44 +94,159 @@ realtor_data <- realtor_data %>%
   mutate(annual_property_tax = as.numeric
          (gsub('[$, (CAD)]', '', annual_property_tax))) 
 
-#group exterior type
+
+#extract exterior_finish
 realtor_data <- realtor_data %>%
-  mutate(exterior_finish = case_when(
-    str_detect(exterior_finish, 'Aluminum')~ "Aluminum",
-    str_detect(exterior_finish, 'Brick')~ "Brick",
-    str_detect(exterior_finish, 'Concrete')~ "Concrete",
-    str_detect(exterior_finish, 'Metal')~ "Metal",
-    str_detect(exterior_finish, 'Stone')~ "Stone",
-    TRUE ~ exterior_finish
+  mutate(exterior_brick = case_when(
+    str_detect(exterior_finish, 'Brick')~ 'TRUE',
+    TRUE ~ 'FALSE'
   ))
 
-#extract car wash feature
 realtor_data <- realtor_data %>%
-  mutate(feature_car_wash = case_when(
+  mutate(exterior_concrete = case_when(
+    str_detect(exterior_finish, 'Concrete')~ 'TRUE',
+    TRUE ~ 'FALSE'
+  ))
+
+
+realtor_data <- realtor_data %>%
+  mutate(exterior_other = case_when(
+    str_detect(exterior_finish, "Aluminum|Stone|Metal|Other")~ 'TRUE',
+    TRUE ~ 'FALSE'
+  ))
+
+
+#extract building_amenities attributes
+realtor_data <- realtor_data %>%
+  mutate(building_amenities_car_wash = case_when(
     str_detect(building_amenities, 'Car')~ 'TRUE',
     TRUE ~ 'FALSE'
   ))
 
-#extract exercise center feature
 realtor_data <- realtor_data %>%
-  mutate(feature_exercise_center = case_when(
+  mutate(building_amenities_exercise_center = case_when(
     str_detect(building_amenities, 'Exercise')~ 'TRUE',
     TRUE ~ 'FALSE'
   ))
 
-#extract storage feature
 realtor_data <- realtor_data %>%
-  mutate(feature_storge = case_when(
+  mutate(building_amenities_storge = case_when(
     str_detect(building_amenities, 'Storage')~ 'TRUE',
     TRUE ~ 'FALSE'
   ))
 
+realtor_data <- realtor_data %>%
+  mutate(building_amenities_security = case_when(
+    str_detect(building_amenities, 'Security')~ 'TRUE',
+    TRUE ~ 'FALSE'
+  ))
+
+realtor_data <- realtor_data %>%
+  mutate(building_amenities_party_room = case_when(
+    str_detect(building_amenities, 'Party')~ 'TRUE',
+    TRUE ~ 'FALSE'
+  ))
+
+realtor_data <- realtor_data %>%
+  mutate(building_amenities_recreation_centre = case_when(
+    str_detect(building_amenities, 'Recreation')~ 'TRUE',
+    TRUE ~ 'FALSE'
+  ))
+
+realtor_data <- realtor_data %>%
+  mutate(building_amenities_sauna = case_when(
+    str_detect(building_amenities, 'Sauna')~ 'TRUE',
+    TRUE ~ 'FALSE'
+  ))
+
+realtor_data <- realtor_data %>%
+  mutate(building_guest_suit = case_when(
+    str_detect(building_amenities, 'Guest')~ 'TRUE',
+    TRUE ~ 'FALSE'
+  ))
+
+
+
+#extract parking type attributes
+
+realtor_data <- realtor_data %>%
+  mutate(parking_type_garage = case_when(
+    str_detect(parking_type, 'Garage')~ 'TRUE',
+    TRUE ~ 'FALSE'
+  ))
+
+realtor_data <- realtor_data %>%
+  mutate(parking_type_visitor_parking = case_when(
+    str_detect(parking_type, 'Visitor')~ 'TRUE',
+    TRUE ~ 'FALSE'
+  ))
+
+realtor_data <- realtor_data %>%
+  mutate(parking_type_undergroung = case_when(
+    str_detect(parking_type, 'Underground')~ 'TRUE',
+    TRUE ~ 'FALSE'
+  ))
+
+#extract feature attributes
+
+realtor_data <- realtor_data %>%
+  mutate(feature_calcony = case_when(
+    str_detect(features, 'Balcony')~ 'TRUE',
+    TRUE ~ 'FALSE'
+  ))
+
+
+#extract amenities nearby attributes
+realtor_data <- realtor_data %>%
+  mutate(amenities_nearby_hospital = case_when(
+    str_detect(amenities_nearby, 'Hospital')~ 'TRUE',
+    TRUE ~ 'FALSE'
+  ))
+
+realtor_data <- realtor_data %>%
+  mutate(amenities_nearby_park = case_when(
+    str_detect(amenities_nearby, 'Park')~ 'TRUE',
+    TRUE ~ 'FALSE'
+  ))
+
+realtor_data <- realtor_data %>%
+  mutate(amenities_nearby_public_transit = case_when(
+    str_detect(amenities_nearby, 'Transit')~ 'TRUE',
+    TRUE ~ 'FALSE'
+  ))
+
+realtor_data <- realtor_data %>%
+  mutate(amenities_nearby_schools = case_when(
+    str_detect(amenities_nearby, ' Schools')~ 'TRUE',
+    TRUE ~ 'FALSE'
+  ))
+
+realtor_data <- realtor_data %>%
+  mutate(amenities_nearby_marina = case_when(
+    str_detect(amenities_nearby, ' Marina')~ 'TRUE',
+    TRUE ~ 'FALSE'
+  ))
+
+
+
+
+
+
 #fit linear regression model
-full_model <- lm(price~., data=na.omit(realtor_data))
+
+realtor_data <- realtor_data %>%
+  dplyr::select(-building_type, -building_amenities, -features, 
+                -exterior_finish, -parking_type, -amenities_nearby
+                )
+
+
+
+full_model <- lm(log(price)~., data=na.omit(realtor_data))
 
 backward_selection <- stats::step(full_model) 
 
 summary(backward_selection)
+
 
 
 #test model assumptions
@@ -139,6 +260,16 @@ test <- data.frame(
   BIC = BIC(best_model)
 )
 glimpse(test)
+
+realtor_data %>%
+  filter(price<3000000) %>%
+  ggplot(aes(x = price, na.rm = TRUE))+
+  xlab("Total earning") +
+  ylab("Count") +
+  ggtitle("Distribution of Total earning") +
+  geom_histogram(binwidth = 30000, colour="black", fill="white")+
+  scale_x_continuous(labels = scales::comma)
+
 
 par(mfrow=c(2,2))
 plot(best_model)
