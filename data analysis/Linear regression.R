12fbe3c0e99f3d3bc08972ba89b6058f08c574ef
Siyuan Shang <cjbbt12@gmail.com>
2021-11-28 13:59:06 -0500

    Use glm instead lm, fillter property tax=0, remove banner attribute, model R^2=0.86

--- data analysis/Linear regression.R
+++ data analysis/Linear regression.R
@@ -52,7 +52,7 @@ raw_data <- raw_data %>%
 realtor_data <- raw_data %>%
   dplyr::select(price, building_type, parking_type,  
                 bathrooms, bedrooms, building_amenities, has_photo, photos, 
-                has_CMHCFTHBBanner, features, cooling, heating_type, community_name, 
+                features, cooling, heating_type, community_name, 
                 time_length, above_grade, maintenance_fees, annual_property_tax,
                 exterior_finish, amenities_nearby
                 )
@@ -65,8 +65,7 @@ realtor_data <- realtor_data %>%
   ))
 
 realtor_data <- realtor_data %>%
-  filter(price<3000000 && price>100000)  %>%
-  filter(building_type == 'Apartment')
+  filter(price<2999000, price>100000, building_type == 'Apartment')
 
 #group heating type
 realtor_data <- realtor_data %>%
@@ -190,7 +189,7 @@ realtor_data <- realtor_data %>%
 #extract feature attributes
 
 realtor_data <- realtor_data %>%
-  mutate(feature_calcony = case_when(
+  mutate(feature_balcony = case_when(
     str_detect(features, 'Balcony')~ 'TRUE',
     TRUE ~ 'FALSE'
   ))
@@ -228,20 +227,49 @@ realtor_data <- realtor_data %>%
   ))
 
 
+#extract significant community name attributes
+realtor_data <- realtor_data %>%
+  mutate(cname_black_creek = case_when(
+    str_detect(community_name, 'Black')~ 'TRUE',
+    TRUE ~ 'FALSE'
+  ))
 
+realtor_data <- realtor_data %>%
+  mutate(cname_downsview_roding-CFB = case_when(
+    str_detect(community_name, 'Downsview')~ 'TRUE',
+    TRUE ~ 'FALSE'
+  ))
 
+realtor_data <- realtor_data %>%
+  mutate(cname_l_amoreaux = case_when(
+    str_detect(community_name, 'Amoreaux')~ 'TRUE',
+    TRUE ~ 'FALSE'
+  ))
+
+realtor_data <- realtor_data %>%
+  mutate(cname_steeles = case_when(
+    str_detect(community_name, 'Steeles')~ 'TRUE',
+    TRUE ~ 'FALSE'
+  ))
 
 
 #fit linear regression model
 
 realtor_data <- realtor_data %>%
+  mutate(annual_property_tax = ifelse(annual_property_tax > 2, annual_property_tax, NA)) %>%
+  mutate(maintenance_fees = ifelse(maintenance_fees > 2, maintenance_fees, NA))
+
+
+realtor_data <- realtor_data %>%
   dplyr::select(-building_type, -building_amenities, -features, 
-                -exterior_finish, -parking_type, -amenities_nearby
+                -exterior_finish, -parking_type, -amenities_nearby,
+                -above_grade, -community_name
                 )
 
 
+#realtor_data<- realtor_data[-360,]
 
-full_model <- lm(log(price)~., data=na.omit(realtor_data))
+full_model <- glm(price~., data=na.omit(realtor_data), family = "gaussian")
 
 backward_selection <- stats::step(full_model) 
 
@@ -249,9 +277,20 @@ summary(backward_selection)
 
 
 
+log_model <- glm(price~., data=na.omit(realtor_data), family = Gamma(link="log"))
+
+log_backward_selection <- stats::step(log_model)
+
+
+
+
+
 #test model assumptions
 best_model <- backward_selection
 
+print("Non data transformation linear regression model")
+summary(backward_selection)
+print("Model assumption for non data transformation model")
 test <- data.frame(
   R2 = modelr::rsquare(best_model, data = na.omit(realtor_data)),
   RMSE = modelr::rmse(best_model, data = na.omit(realtor_data)),
@@ -261,8 +300,21 @@ test <- data.frame(
 )
 glimpse(test)
 
+
+print("Log data transformation linear regression model")
+summary(log_backward_selection)
+print("Model assumption for non data transformation model")
+test2 <- data.frame(
+  R2 = modelr::rsquare(log_backward_selection, data = na.omit(realtor_data)),
+  RMSE = modelr::rmse(log_backward_selection, data = na.omit(realtor_data)),
+  MAE = modelr::mae(log_backward_selection, data = na.omit(realtor_data)),
+  AIC = AIC(log_backward_selection),
+  BIC = BIC(log_backward_selection)
+)
+glimpse(test2)
+
+
 realtor_data %>%
-  filter(price<3000000) %>%
   ggplot(aes(x = price, na.rm = TRUE))+
   xlab("Total earning") +
   ylab("Count") +
@@ -275,4 +327,19 @@ par(mfrow=c(2,2))
 plot(best_model)
 
 
+par(mfrow=c(2,2))
+plot(log_backward_selection)
+with(summary(log_backward_selection), 1 - deviance/null.deviance)
+
+
+#simple model
+simple_model <- glm(price~bathrooms+maintenance_fees+annual_property_tax, data=na.omit(realtor_data), family = "gaussian")
+test3 <- data.frame(
+  R2 = modelr::rsquare(simple_model, data = na.omit(realtor_data)),
+  RMSE = modelr::rmse(simple_model, data = na.omit(realtor_data)),
+  MAE = modelr::mae(simple_model, data = na.omit(realtor_data)),
+  AIC = AIC(simple_model),
+  BIC = BIC(simple_model)
+)
+glimpse(test3)
 
